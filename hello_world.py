b95d758a60a2a967e59968ac3faee3a0868b947c
Zaja91 <yagos.terni@gmail.com>
2020-03-02 17:43:14 +0100

    2nd

---
+++ hello_world.py
@@ -0,0 +1,7 @@
+message = "hello world!"
+print(message)
+print(message.title())
+first_name = "andrei"
+last_name = "ilinca"
+full_name = f"{first_name.title()} {last_name.title()}"
+print(full_name)
