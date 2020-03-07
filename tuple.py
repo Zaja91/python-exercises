8ee26be5d4620f9916236a73f4c29544a203916d
Zaja91 <yagos.terni@gmail.com>
2020-03-07 10:20:22 +0100

    5day

---
+++ tuple.py
@@ -0,0 +1,8 @@
+thirstuple =('apple', 'banana', 'mango')
+print(thirstuple[0])
+arr = ['apple', 'banana', 'golden apple']
+print(arr[2])
+data = ['ACME', 50, 91.1, (2012, 12, 21)]
+name, shares, price, date = data
+print(data)
+print(name)
